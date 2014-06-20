78edf0ff46f1b0dc5ea1bc8cdf867e27285cc9ee
Bill DeLude <Bill.DeLude@RelayHealth.com>
2014-06-20 14:00:53 -0700

    Page working, mocking not so much

--- PhoneCat.Tests/jasmine/samples/PlayerSpec.js
+++ PhoneCat.Tests/jasmine/samples/PlayerSpec.js
@@ -2,80 +2,81 @@
 /// <reference path="Song.js"/>
 /// <reference path="~/jasmine/jasmine.js"/>
 /// <reference path="~/jasmine/jasmine-html.js"/>
+/// <reference path="~/jasmine/boot.js" />
 
 describe("Player", function () {
-  var player;
-  var song;
+    var player;
+    var song;
 
-  var addPlayerMatchers = function () {
-      jasmine.addMatchers({
-          toBePlaying: function () {
-              return {
-                  compare: function (actual, expected) {
-                      var actualPlayer = actual;
+    var addPlayerMatchers = function () {
+        window.jasmine.addMatchers({
+            toBePlaying: function () {
+                return {
+                    compare: function (actual, expected) {
+                        var actualPlayer = actual;
 
-                      return {
-                          pass: actualPlayer.currentlyPlayingSong === expected && actualPlayer.isPlaying
-                      }
-                  }
-              };
-          }
-      });
-  }
+                        return {
+                            pass: actualPlayer.currentlyPlayingSong === expected && actualPlayer.isPlaying
+                        }
+                    }
+                };
+            }
+        });
+    }
 
-  beforeEach(function () {
-    player = new Player();
-    song = new Song();
-      addPlayerMatchers();
-  });
-
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
+    beforeEach(function () {
+        player = new Player();
+        song = new Song();
         addPlayerMatchers();
     });
 
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
+    it("should be able to play a Song", function () {
+        player.play(song);
+        expect(player.currentlyPlayingSong).toEqual(song);
 
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
+        //demonstrates use of custom matcher
+        expect(player).toBePlaying(song);
     });
 
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
+    describe("when song has been paused", function () {
+        beforeEach(function () {
+            player.play(song);
+            player.pause();
+            addPlayerMatchers();
+        });
+
+        it("should indicate that the song is currently paused", function () {
+            expect(player.isPlaying).toBeFalsy();
+
+            // demonstrates use of 'not' with a custom matcher
+            expect(player).not.toBePlaying(song);
+        });
+
+        it("should be possible to resume", function () {
+            player.resume();
+            expect(player.isPlaying).toBeTruthy();
+            expect(player.currentlyPlayingSong).toEqual(song);
+        });
     });
-  });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
+    // demonstrates use of spies to intercept and test method calls
+    it("tells the current song if the user has made it a favorite", function () {
+        spyOn(song, 'persistFavoriteStatus');
 
-    player.play(song);
-    player.makeFavorite();
+        player.play(song);
+        player.makeFavorite();
 
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
+        expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
     });
-  });
+
+    //demonstrates use of expected exceptions
+    describe("#resume", function () {
+        it("should throw an exception if song is already playing", function () {
+            player.play(song);
+
+            expect(function () {
+                player.resume();
+            }).toThrowError("song is already playing");
+        });
+    });
 });
---
+++ PhoneCat.Tests/js/controllersSpec.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="~/jasmine/jasmine.js"/>
+/// <reference path="~/jasmine/jasmine-html.js"/>
+/// <reference path="~/jasmine/boot.js" />
+/// <reference path="/../PhoneCat/scripts/angular.min.js" />
+/// <reference path="/../PhoneCat/scripts/angular-mocks.js" />
+/// <reference path="/../PhoneCat/js/controllers.js" />
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function () {
+
+    describe('controller', function () {
+        it("should do something", function () { });
+    });
+
+    describe('PhoneListCtrl', function () {
+
+        beforeEach(module('phonecatApp'));
+
+        it('should create "phones" model with 3 phones', inject(function ($controller) {
+            var scope = {},
+                ctrl = $controller('PhoneListCtrl', { $scope: scope });
+
+            expect(scope.phones.length).toBe(3);
+        }));
+
+    });
+
+});
